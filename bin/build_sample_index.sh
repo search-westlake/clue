d5423ab2e3460f3994c90a6113a18452395433ca
John Wang <john.wang@gmail.com>
2025-12-28 10:30:54 -0800

    added to script to load from plugins dir (#95)
    Co-authored-by: John Wang <cocobeach1911@gmail.com>

--- bin/build_sample_index.sh
+++ bin/build_sample_index.sh
@@ -4,10 +4,16 @@ bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
 lib=$bin/../build/libs
+plugins_dir=$bin/../plugins
 
 HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
 JAVA_OPTS=""
 
 MAIN_CLASS="io.dashbase.clue.test.BuildSampleIndex"
 
-java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp "$lib/*" $MAIN_CLASS $bin/../src/main/resources/cars.json $@
+CLASSPATH="$lib/*"
+if [[ -d "$plugins_dir" ]]; then
+  CLASSPATH="$CLASSPATH:$plugins_dir/*"
+fi
+
+java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp "$CLASSPATH" $MAIN_CLASS $bin/../src/main/resources/cars.json $@
